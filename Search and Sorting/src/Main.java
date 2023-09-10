a9376187404c1ddb1fd16893a3e51c5cbcff8dc5
Jhung-25 <jhung25@windwardschool.org>
2023-09-10 16:58:42 -0700

    Fix Binary Serach
    Tried the opposite way of binary search then realized that i was comparing the number to the index instead of arr[midPoint]

--- Search and Sorting/src/Main.java
+++ Search and Sorting/src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 4, 4, 5, 6};
-//        System.out.println(sequentialSearch(4, arr));
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println(sequentialSearch(4, arr));
         System.out.println(binarySearch(6,arr));
     }
 
@@ -19,19 +19,19 @@ public class Main {
     public static int binarySearch(int tarNum, int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        int midPoint = start+end/2;
+        int midPoint = 0;
 
         while (start <= end) {
-            midPoint=(start+end)/2;
-            if(midPoint==tarNum){
-                return midPoint-1;
+            midPoint=start+(end-start) /2;
+            if(arr[midPoint]==tarNum){
+                return midPoint;
 
             }
-            if (arr[midPoint]>tarNum) {
-                end=midPoint-1;
+            if (arr[midPoint]<tarNum) {
+                start=midPoint+1;
             }
             else{
-                start=midPoint+1;
+                end=midPoint-1;
             }
 
         }
