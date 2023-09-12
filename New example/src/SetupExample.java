efe2930cc0601aaf4bb2ab40768df8830c8bb458
Jhung-25 <jhung25@windwardschool.org>
2023-09-12 12:54:05 -0700

    Finishing the constructors

--- New example/src/SetupExample.java
+++ New example/src/SetupExample.java
@@ -8,7 +8,7 @@ public class SetupExample extends PApplet{
     }
 
     public void settings() {
-        size(1060,1010);
+        size(1016,1010);
     }
 
     public void setup() {
@@ -17,31 +17,32 @@ public class SetupExample extends PApplet{
         // that represents red...
         // ... when you look in the constructor of ParentShape
         // the second parameter is declared as an int.
-        someShape = new jHungShape(100,100,300,300,500);
+        someShape = new jHungShape(100,100,300,300,0,0,0,0);
         someShape.draw();
-        rectShape exShape = new rectShape(100,100,100,100,100);
+        rectShape exShape = new rectShape(100,100,100,100,100,40,50,14);
+
+//        fill(255);
+//        background(255);
+//        fill(0);
+//        ellipse(508,467,640,640);
+//        fill(35,69,149,255);
+//        rect(56,530,324,96);
+//        fill(255);
+//        rect(235,643,900,8);
+//        fill(202,0,15,255);
+//        rect(606,339,335,329);
+//        fill(255);
+//        rect(167,589,673,12);
+//        fill(0);
+//        rect(68,799,887,60);
+//        fill(255);
+//        rect(285,815,422,13);
+//        rect(408,339,8,405);
+//        rect(376,396,40,32);
+//        fill(255,217,0,255);
+//        rect(426,217,9,500);
+//        fill(255);
         exShape.draw();
-        fill(255);
-        background(255);
-        fill(0);
-        ellipse(508,467,640,640);
-        fill(35,69,149,255);
-        rect(56,530,324,96);
-        fill(255);
-        rect(235,643,900,8);
-        fill(202,0,15,255);
-        rect(606,339,335,329);
-        fill(255);
-        rect(167,589,673,12);
-        fill(0);
-        rect(68,799,887,60);
-        fill(255);
-        rect(285,815,422,13);
-        rect(408,339,8,405);
-        rect(376,396,40,32);
-        fill(255,217,0,255);
-        rect(426,217,9,500);
-        fill(255);
     }
 
     /*Declare a class INSIDE of the class
@@ -50,8 +51,8 @@ public class SetupExample extends PApplet{
     inside of your Shape class.
      */
     class jHungShape {
-         int x,y,w,h;
-        int color;
+        int x,y,w,h;
+        int color1,color2,color3,colorA;
         int getX() {
             return x;
         }
@@ -72,13 +73,19 @@ public class SetupExample extends PApplet{
 //        shapeColor = c;
         }
 
-        jHungShape(int x,int y, int w, int h, int color) {
+        jHungShape(int x,int y, int w, int h, int color1,int color2,int color3,int colorA) {
             this.x = x;
-            this.color = color;
+            this.y =y;
+            this.w =w;
+            this.h = h;
+            this.color1 = color1;
+            this.color2=color2;
+            this.color3=color3;
+            this.colorA=colorA;
         }
 
         public void draw() {
-            fill(color);
+            fill(color1,color2,color3,colorA);
             ellipse(x, y, w,h);
         }
     }
@@ -89,15 +96,15 @@ public class SetupExample extends PApplet{
     one too!
      */
     class rectShape extends jHungShape {
-        rectShape(int x,int y, int w, int h, int c) {
+        rectShape(int x,int y, int w, int h, int c1,int c2, int c3, int cA) {
             // call the ParentShape constructor
             // which will setup the properties
             // this color is cyan, btw.
-            super(x,y,w,h,c);
+            super(x,y,w,h,c1,c2,c3,cA);
         }
 
         public void draw() {
-            fill(color);
+            fill(color1,color2,color3,colorA);
             rect(x,y,w,h);
         }
     }
