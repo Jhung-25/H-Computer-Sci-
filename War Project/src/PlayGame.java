2b7d5fcb585199ba30790569a4d0df5568d36a63
Jhung-25 <jhung25@windwardschool.org>
2023-09-24 14:47:00 -0700

    Update PlayGame.java

--- War Project/src/PlayGame.java
+++ War Project/src/PlayGame.java
@@ -2,14 +2,15 @@ import processing.core.*;
 
 import java.util.ArrayList;
 
-public class PlayGame extends PApplet{
-    int buttonX,buttonY,buttonW, buttonH;
-    int player1WinCount=0;
-    int player2WinCount=0;
+public class PlayGame extends PApplet {
+    int buttonX, buttonY, buttonW, buttonH;
+    int player1WinCount = 0;
+    int player2WinCount = 0;
 
     WarGame warGame;
     ArrayList<Cards> player1Deck;
     ArrayList<Cards> player2Deck;
+
     public static void main(String[] args) {
         PApplet.main(PlayGame.class);
 
@@ -17,8 +18,9 @@ public class PlayGame extends PApplet{
     }
 
     public void settings() {
-        size(900,900);
+        size(900, 900);
     }
+
     public void setup() {
         warGame = new WarGame(); // Create an instance of WarGame
         ArrayList<Cards> deck = new ArrayList<Cards>();
@@ -27,29 +29,27 @@ public class PlayGame extends PApplet{
         player1Deck = new ArrayList<Cards>(warGame.p1Deck(shuffled));
         player2Deck = new ArrayList<Cards>(warGame.p2Deck(shuffled));
         warGame.war(player2Deck, player1Deck, 1);
-        buttonW=100;
-        buttonH=40;
-        buttonX=width/2-buttonW/2;
-        buttonY=height/2 - buttonH/2;
-
+        buttonW = 100;
+        buttonH = 40;
+        buttonX = width / 2 - buttonW / 2;
+        buttonY = height / 2 - buttonH / 2;
 
 
     }
 
     public void draw() {
         background(255);
-        if(mouseX>=buttonX&&mouseX<=buttonX+buttonW&&mouseY>=buttonY&& mouseY<=buttonY+buttonH&&mousePressed){
-            fill(255,180,30);
+        if (mouseX >= buttonX && mouseX <= buttonX + buttonW && mouseY >= buttonY && mouseY <= buttonY + buttonH && mousePressed) {
+            fill(255, 180, 30);
             textSize(10);
 
 
-        }
-        else{
-            fill(100,50,80);
+        } else {
+            fill(100, 50, 80);
             textSize(20);
 
         }
-        rect(buttonX,buttonY,buttonW,buttonH,5);
+        rect(buttonX, buttonY, buttonW, buttonH, 5);
 
 
         fill(255);
@@ -60,35 +60,30 @@ public class PlayGame extends PApplet{
 
         fill(0);
         textSize(20);
-        text(mouseX+","+mouseY,mouseX+10,mouseY + 10);
-        text("Let's Play War",440,155);
-        text("Player1 Win Count:"+player1WinCount,260,400);
-        text("Player2 Win Count:"+player2WinCount,620,400);
-        text("Player 1 Deck Size:"+player1Deck.size(),100,10);
-        text("Player 2 Deck Size:"+player2Deck.size(),100,50);
+        text(mouseX + "," + mouseY, mouseX + 10, mouseY + 10);
+        text("Let's Play War", 440, 155);
+        text("Player1 Win Count:" + player1WinCount, 260, 400);
+        text("Player2 Win Count:" + player2WinCount, 620, 400);
+        text("Player 1 Deck Size:" + player1Deck.size(), 100, 10);
+        text("Player 2 Deck Size:" + player2Deck.size(), 100, 50);
         player2Deck.get(1).draw(this, 200);
         player1Deck.get(1).draw(this, 550);
 
 
     }
+
     public void mousePressed() {
         // Check if mouse is pressed within the bounds of the rectangle
-        if(mouseX > width/2 - 60 && mouseX < width/2 + 60 && mouseY > height/2 - 20 && mouseY < height/2 + 20) {
+        if (mouseX > width / 2 - 60 && mouseX < width / 2 + 60 && mouseY > height / 2 - 20 && mouseY < height / 2 + 20) {
             println("Button Pressed!"); // Print message to console if the button is pressed
-            if(warGame.war(player2Deck, player1Deck, 1)==1){
+            if (warGame.war(player2Deck, player1Deck, 1) == 1) {
                 player1WinCount++;
             }
-            if(warGame.war(player2Deck, player1Deck, 1)==2){
+            if (warGame.war(player2Deck, player1Deck, 1) == 2) {
                 player2WinCount++;
             }
-            try{
-                warGame.war(player2Deck, player1Deck, 1);
-                if()
-            }
-
-
 
         }
-    }
 
+    }
 }
