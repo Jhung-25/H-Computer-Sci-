b357c703c88a5ee63e98bf6315e5791fa3c98ce9
Jhung-25 <jhung25@windwardschool.org>
2023-10-04 10:03:10 -0700

    Update TesterClass.java

--- Search and Sorting/src/TesterClass.java
+++ Search and Sorting/src/TesterClass.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 public class TesterClass {
     public static void main (String[]args){
-        for(int i=10;i<10010;i+=100) {
-            algoTester(5, i);
-        }
+//        for(int i=0;i<1000;i+=100) {
+//            algoTester(5, i);
+//        }
+        ArrayList<Integer> arr= new ArrayList<Integer>(Arrays.asList(50,20,1,60,42,34,532,53,23,23));
+
+        selectionSortCompNum(arr);
     }
     public static void algoTester(int numRuns,int size){
         //numRns = num of runs
@@ -32,10 +36,10 @@ public class TesterClass {
         }
         return arr;
     }
-    public static int insertionSortCompNum(ArrayList<Integer> arrList) {
+    public static long insertionSortCompNum(ArrayList<Integer> arrList) {
         int temp;
         int newPlace;
-        int numOfComp=0;
+        long numOfComp=0;
         for (int i = 1; i < arrList.size(); i++) {
             temp = arrList.get(i);
             newPlace = i;
@@ -48,10 +52,10 @@ public class TesterClass {
         }
         return numOfComp;
     }
-    public static int selectionSortCompNum(ArrayList<Integer> arrList) {
+    public static long selectionSortCompNum(ArrayList<Integer> arrList) {
         int indexOfLow;
         int placeHolder;
-        int numOfComp=0;
+        long numOfComp=0;
         for (int j = 0; j < arrList.size() - 1; j++) {
             indexOfLow = j;
             for (int i = j + 1; i < arrList.size(); i++) {
@@ -64,6 +68,8 @@ public class TesterClass {
             arrList.set(j, arrList.get(indexOfLow));
             arrList.set(indexOfLow, placeHolder);
         }
+        System.out.println(arrList.toString());
+
         return numOfComp;
     }
 }
